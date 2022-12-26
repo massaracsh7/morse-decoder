e608d02ca432253d6b51833e5dd94b6f922963fc
Your Name <you@example.com>
2022-12-26 21:27:52 +0300

    feat: add decode

--- src/index.js
+++ src/index.js
@@ -38,9 +38,25 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+  let str = expr;
+  let changeStr = '';
+  for (i = 0; i < str.length; i = i + 10) {
+    let letter = str.substr(i, 10);
+    if (letter == '**********') {
+      changeStr = changeStr + ' ';
+    } else {
+    let n = letter.indexOf('1');
+    let letter2 = letter.slice(n);
+    let letter3 = letter2.replace(/11/g, "-");
+    let letter4 = letter3.replace(/10/g, ".");
+    changeStr = changeStr + MORSE_TABLE[letter4];
+    }
+  }
+  return changeStr;
 }
+
 
 module.exports = {
     decode
 }
+
